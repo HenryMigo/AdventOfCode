e8065628b36ddc658ecbd0500fedf4e73e40ef0e
Henry Migo <henryeamigo@gmail.com>
2022-09-26 21:56:16 +0100

    implementing a library for all the days

---
+++ Rust/2015/AdventOfCode/advent_of_code_library/src/dayone.rs
@@ -0,0 +1,47 @@
+pub mod dayone {
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
+
+    pub fn part_one() -> i32 {
+        let file = BufReader::new(File::open("input.txt").expect("open failed"));
+
+        let mut floor = 0;
+
+        for line in file.lines() {
+            for character in line.expect("lines failed").chars() {
+                if character == '(' {
+                    floor = floor + 1;
+                } else if character == ')' {
+                    floor = floor - 1;
+                }
+            }
+        }
+
+        return floor;
+    }
+
+    pub fn part_two() -> i32 {
+        let file = BufReader::new(File::open("input.txt").expect("open failed"));
+
+        let mut floor = 0;
+        let mut character_number = 0;
+
+        for line in file.lines() {
+            for character in line.expect("lines failed").chars() {
+                character_number = character_number + 1;
+
+                if character == '(' {
+                    floor = floor + 1;
+                } else if character == ')' {
+                    floor = floor - 1;
+                }
+
+                if floor == -1 {
+                    return character_number;
+                }
+            }
+        }
+
+        return character_number;
+    }
+}
--- Rust/2015/AdventOfCode/src/main.rs
+++ Rust/2015/AdventOfCode/src/main.rs
@@ -1,53 +1,9 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use adventofcodelibrary::dayone;
 
 fn main() {
-    let part_one = part_one();
-    let part_two = part_two();
+    let part_one = dayone::part_one();
+    let part_two = dayone::part_two();
 
     println!("Part one: {}", part_one);
     println!("Part two: {}", part_two);
-}
-
-fn part_one() -> i32 {
-    let file = BufReader::new(File::open("input.txt").expect("open failed"));
-
-    let mut floor = 0;
-
-    for line in file.lines() {
-        for character in line.expect("lines failed").chars() {
-            if character == '(' {
-                floor = floor + 1;
-            } else if character == ')' {
-                floor = floor - 1;
-            }
-        }
-    }
-
-    return floor;
-}
-
-fn part_two() -> i32 {
-    let file = BufReader::new(File::open("input.txt").expect("open failed"));
-
-    let mut floor = 0;
-    let mut character_number = 0;
-
-    for line in file.lines() {
-        for character in line.expect("lines failed").chars() {
-            character_number = character_number + 1;
-
-            if character == '(' {
-                floor = floor + 1;
-            } else if character == ')' {
-                floor = floor - 1;
-            }
-
-            if floor == -1 {
-                return character_number;
-            }
-        }
-    }
-
-    return character_number;
 }
