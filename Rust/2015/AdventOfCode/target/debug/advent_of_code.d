f33d4b6b5c4914e5e6df1492517f72586ae97b84
Henry Migo <henryeamigo@gmail.com>
2022-09-26 18:59:04 +0100

    Actually added to gitignore

---
+++ Rust/2015/AdventOfCode/target/debug/advent_of_code.d
@@ -0,0 +1,1 @@
+D:\dev\AdventOfCode\Rust\2015\AdventOfCode\target\debug\advent_of_code.exe: D:\dev\AdventOfCode\Rust\2015\AdventOfCode\src\main.rs
---
+++ Rust/2015/AdventOfCode/target/debug/deps/advent_of_code-c3a3b5ac59470053.d
@@ -0,0 +1,5 @@
+d:\dev\AdventOfCode\Rust\2015\AdventOfCode\target\debug\deps\advent_of_code-c3a3b5ac59470053.rmeta: src\main.rs
+
+d:\dev\AdventOfCode\Rust\2015\AdventOfCode\target\debug\deps\advent_of_code-c3a3b5ac59470053.d: src\main.rs
+
+src\main.rs:
