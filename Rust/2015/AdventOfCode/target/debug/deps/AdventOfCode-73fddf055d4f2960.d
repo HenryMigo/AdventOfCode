cf6a020e9f040d9688ad171cbb1ccd8287022aa0
Henry Migo <henryeamigo@gmail.com>
2022-09-26 18:47:31 +0100

    Initial setup

---
+++ Rust/2015/AdventOfCode/target/debug/deps/AdventOfCode-73fddf055d4f2960.d
@@ -0,0 +1,5 @@
+d:\dev\AdventOfCode\Rust\2015\AdventOfCode\target\debug\deps\AdventOfCode-73fddf055d4f2960.rmeta: src\main.rs
+
+d:\dev\AdventOfCode\Rust\2015\AdventOfCode\target\debug\deps\AdventOfCode-73fddf055d4f2960.d: src\main.rs
+
+src\main.rs:
---
+++ Rust/2015/AdventOfCode/target/debug/deps/AdventOfCode-a17c114276cf7fb6.d
@@ -0,0 +1,5 @@
+d:\dev\AdventOfCode\Rust\2015\AdventOfCode\target\debug\deps\AdventOfCode-a17c114276cf7fb6.rmeta: src\main.rs
+
+d:\dev\AdventOfCode\Rust\2015\AdventOfCode\target\debug\deps\AdventOfCode-a17c114276cf7fb6.d: src\main.rs
+
+src\main.rs:
