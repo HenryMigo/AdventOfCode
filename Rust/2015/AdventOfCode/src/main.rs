9cb1a2b496f17623916163424e0f8ce8e909af06
Henry Migo <henryeamigo@gmail.com>
2022-09-28 18:56:05 +0100

    Implemented some basic printing

--- Rust/2015/AdventOfCode/src/main.rs
+++ Rust/2015/AdventOfCode/src/main.rs
@@ -1,11 +1,24 @@
-use adventofcodelibrary::*;
+use adventofcodelibrary::{dayone, daytwo};
 
 fn main() {
     // TODO: Implement a menu to select which day to run
-    let day_one_part_one = DayOne::part_one();
-    let day_one_part_two = DayOne::part_two();
-    let day_two_part_one = DayTwo::part_one();
+    let day_one_part_one = dayone::DayOne::part_one();
+    let day_one_part_two = dayone::DayOne::part_two();
+    let day_two_part_one = daytwo::DayTwo::part_one();
+    let day_two_part_two = daytwo::DayTwo::part_two();
 
-    println!("Part one: {}", day_one_part_one);
-    println!("Part two: {}", day_one_part_two);
+    // TODO: Do this better
+    println!("----------------------");
+    println!("Advent of code - 2015");
+    println!("----------------------");
+    print_result(day_one_part_one, day_one_part_two, 1);
+    print_result(day_one_part_two, day_one_part_two, 2);
+    print_result(day_two_part_one, day_two_part_one, 1);
+    print_result(day_two_part_two, day_two_part_two, 2);
+}
+
+fn print_result(part_one: i32, part_two: i32, day: i32) {
+    println!("Day {}: Part {}: Result {}", day, 1, part_one);
+    println!("Day {}: Part {}: Result {}", day, 2, part_two);
+    println!("----------------------");
 }
