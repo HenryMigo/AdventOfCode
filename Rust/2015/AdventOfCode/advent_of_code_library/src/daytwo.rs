9afa46ae9064282990a31c0c1a70e41342f68921
Henry Migo <henryeamigo@gmail.com>
2022-09-29 19:04:13 +0100

    Completed day two, forgot it wanted the shortest way round the present

--- Rust/2015/AdventOfCode/advent_of_code_library/src/daytwo.rs
+++ Rust/2015/AdventOfCode/advent_of_code_library/src/daytwo.rs
@@ -38,16 +38,47 @@ impl DayTwo {
         let mut result = 0;
 
         for area in areas {
-            result = result + area;
+            result += area;
         }
 
         return result;
     }
 
     pub fn part_two() -> i32 {
-        _ = BufReader::new(File::open("files/daytwo.txt").expect("open failed"));
+        let file = BufReader::new(File::open("files/daytwo.txt").expect("open failed"));
 
-        -1
+        let mut feet_of_ribbons: Vec<i32> = Vec::new();
+
+        for line in file.lines() {
+            let line_clone = line.expect("line failed").clone();
+            let mut measurements = line_clone.split("x");
+
+            let length = DayTwo::convert_string_to_int(measurements.next().expect("Length failed"));
+            let width = DayTwo::convert_string_to_int(measurements.next().expect("Width failed"));
+            let height = DayTwo::convert_string_to_int(measurements.next().expect("Height failed"));
+
+            let mut shortest_side = length + width;
+
+            if shortest_side > width + height {
+                shortest_side = width + height;
+            }
+
+            if shortest_side > height + length {
+                shortest_side = height + length;
+            }
+
+            let total = (2 * shortest_side) + (length * width * height);
+
+            feet_of_ribbons.push(total);
+        }
+
+        let mut total_feet_of_ribbon = 0;
+
+        for feet_of_ribbon in feet_of_ribbons {
+            total_feet_of_ribbon += feet_of_ribbon;
+        }
+
+        return total_feet_of_ribbon;
     }
 
     fn convert_string_to_int(input: &str) -> i32 {
