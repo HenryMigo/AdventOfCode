0e28607253743d83bc649906300e2d650e977a42
Henry Migo <henryeamigo@gmail.com>
2023-03-18 16:29:39 +0000

    Part 1 of Day 3

---
+++ CSharp/AOC2022/AOC2022.Library/Day3.cs
@@ -0,0 +1,103 @@
+﻿namespace AOC2022.Library
+{
+    public static class Day3
+    {
+        private static readonly Dictionary<char, int> Scores = new()
+        {
+            {'a', 1 },
+            {'b', 2 },
+            {'c', 3 },
+            {'d', 4 },
+            {'e', 5 },
+            {'f', 6 },
+            {'g', 7 },
+            {'h', 8 },
+            {'i', 9 },
+            {'j', 10 },
+            {'k', 11 },
+            {'l', 12 },
+            {'m', 13 },
+            {'n', 14 },
+            {'o', 15 },
+            {'p', 16 },
+            {'q', 17 },
+            {'r', 18 },
+            {'s', 19 },
+            {'t', 20 },
+            {'u', 21 },
+            {'v', 22 },
+            {'w', 23 },
+            {'x', 24 },
+            {'y', 25 },
+            {'z', 26 },
+            {'A', 27 },
+            {'B', 28 },
+            {'C', 29 },
+            {'D', 30 },
+            {'E', 31 },
+            {'F', 32 },
+            {'G', 33 },
+            {'H', 34 },
+            {'I', 35 },
+            {'J', 36 },
+            {'K', 37 },
+            {'L', 38 },
+            {'M', 39 },
+            {'N', 40 },
+            {'O', 41 },
+            {'P', 42 },
+            {'Q', 43 },
+            {'R', 44 },
+            {'S', 45 },
+            {'T', 46 },
+            {'U', 47 },
+            {'V', 48 },
+            {'W', 49 },
+            {'X', 50 },
+            {'Y', 51 },
+            {'Z', 52 },
+        };
+
+        public static int Part_One(List<string> inputData)
+        {
+            var ruckSacks = new List<RuckSack>();
+            var existsInBothSacks = new List<Dictionary<char, int>>();
+
+            foreach(var input in inputData)
+            {
+                ruckSacks.Add(new RuckSack(input));
+            }
+
+            foreach(var sack in ruckSacks)
+            {
+                var existsInBoth = new Dictionary<char, int>();
+                foreach(var item in sack.FirstPart)
+                {
+                    if (sack.SecondPart.Contains(item))
+                    {
+                        if (!existsInBoth.ContainsKey(item))
+                        {
+                            existsInBoth.Add(item, Scores[item]);
+                        }
+                    }
+                }
+
+                existsInBothSacks.Add(existsInBoth);
+            }
+
+            return existsInBothSacks.Select(x => x.Sum(y => y.Value)).Sum();
+        }
+    }
+
+    public class RuckSack
+    {
+        public RuckSack(string input)
+        {
+            FirstPart = input[0..(input.Length / 2)].ToList();
+            SecondPart = input[(input.Length / 2)..input.Length].ToList();
+        }
+
+        public List<char> FirstPart { get; set; }
+        public List<char> SecondPart { get; set; }
+    }
+}
---
+++ CSharp/AOC2022/AOC2022.UnitTests/Day3Tests.cs
@@ -0,0 +1,35 @@
+﻿using AOC2022.Library;
+using FluentAssertions;
+using Xunit;
+
+namespace AOC2022.UnitTests
+{
+    public class Day3Tests
+    {
+        [Fact]
+        public void Day3_Part1_Example()
+        {
+            // Arrange
+            var inputData = "vJrwpWtwJgWrhcsFMMfFFhFp\r\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\r\nPmmdzqPrVvPwwTWBwg\r\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\r\nttgJtRGJQctTZtZT\r\nCrZsJsPPZsGzwwsLwLmpwMDw".Split("\r\n").ToList();
+
+            // Act
+            var result = Day3.Part_One(inputData);
+
+            // Assert
+            result.Should().Be(157);
+        }
+
+        [Fact]
+        public void Day3_Part1()
+        {
+            // Arrange
+            var inputData = File.ReadAllLines("../../../InputData/Day3.txt").ToList();
+
+            // Act
+            var result = Day3.Part_One(inputData);
+
+            // Assert
+            result.Should().Be(7821);
+        }
+    }
+}
